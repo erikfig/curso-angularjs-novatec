46254f6c31a3699507314914342902e533f928fb
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-22 14:35:13 -0300

    Ações do formulário

--- Projetos/Exemplo 2/script.js
+++ Projetos/Exemplo 2/script.js
@@ -6,4 +6,14 @@ form.controller('FormCtrl', ['$scope', function($scope) {
 	$scope.msg['minlength'] = 'Muito curto';
 	$scope.msg['required'] = 'Campo obrigatório';
 	$scope.msg['email'] = 'Informe o Email corretamente.';
+
+	$scope.toSave = function()
+	{
+		$scope.saved = $scope.formData;
+	}
+
+	$scope.reset = function()
+	{
+		$scope.formData = null;
+	}
 }]);
