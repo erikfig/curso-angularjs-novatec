648140c23036f6c9114b00723fab846cefdc8a3c
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-22 15:28:29 -0300

    Formulário finalizado

--- Projetos/Exemplo 2/script.js
+++ Projetos/Exemplo 2/script.js
@@ -1,15 +1,29 @@
 var form = angular.module('form', []);
 
-form.controller('FormCtrl', ['$scope', function($scope) {
+form.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
 
 	$scope.msg = [];
 	$scope.msg['minlength'] = 'Muito curto';
 	$scope.msg['required'] = 'Campo obrigatório';
 	$scope.msg['email'] = 'Informe o Email corretamente.';
+	$scope.msg['idade'] = 'Você precisa ser mais velho.';
+
+	$http.get('http://localhost:8088/formData.json')
+		.success(function(data) {
+			$scope.formData = data;
+		})
+		.error(function(data) {
+			alert('Algo deu errado');
+		});
 
 	$scope.toSave = function()
 	{
 		$scope.saved = $scope.formData;
+		console.log($scope.saved);
+		$http.post('http://localhost:8088/formData.php', {'data':$scope.saved})
+			.success(function(data) {
+				console.log(data);
+			});
 	}
 
 	$scope.reset = function()
@@ -17,3 +31,17 @@ form.controller('FormCtrl', ['$scope', function($scope) {
 		$scope.formData = null;
 	}
 }]);
+
+form.directive('idade', function() {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, elm, attrs, ctrl) {
+			ctrl.$validators.idade = function(modelValue, viewValue) {
+				if (modelValue >= 14)
+					return true;
+				return false
+			};
+		}
+	}
+});
