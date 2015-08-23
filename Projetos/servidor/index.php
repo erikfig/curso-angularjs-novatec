5bd868da70dcb4d85eea80795c120223ae010b1d
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-08-23 10:24:00 -0300

    CRUD REST

--- Projetos/servidor/index.php
+++ Projetos/servidor/index.php
@@ -5,8 +5,34 @@ require 'bootstrap.php';
 use App\Models\User;
 
 $app->get('/', function() {
-	User::where('id', 1)->first();
 	echo 'Hello World';
+});
+
+$app->get('/users', function() {
+	$data = User::get();
+	echo json_encode($data->toArray());
+});
+
+$app->get('/users/:id', function($id) {
+	$data = User::where('id', $id)->first();
+	echo json_encode($data->toArray());
+});
+
+$app->post('/users', function() {
+	$data = User::create($_POST);
+	echo json_encode($data->toArray());
+});
+
+$app->map('/users/:id', function($id) {
+	$data = User::where('id', $id)->first();
+	$data->update($_POST);
+	echo json_encode($data->toArray());
+})->via('post', 'put');
+
+$app->delete('/users/:id', function($id) {
+	$data = User::where('id', $id)->first();
+	$data->delete();
+	echo json_encode(['msg'=>'success']);
 });
 
 $app->run();
